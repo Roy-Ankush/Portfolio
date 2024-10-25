3e89045b44756603f98a1491e5459b88d3d2ae4a
Ankush Roy <146131254+Roy-Ankush@users.noreply.github.com>
2024-10-26 01:08:41 +0530

    Update Contact.jsx

--- client/src/pages/contact/Contact.jsx
+++ client/src/pages/contact/Contact.jsx
@@ -27,7 +27,7 @@ function Contact() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('http://localhost:8000/contact', {
+      const response = await fetch('https://ankush-portfolio-w9wt.onrender.com', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
